827c42a1e90dfcf4faedecd58dbdee3796bdb2e1
mblai35 <mbstrong35@gmail.com>
2016-09-06 22:05:51 -0600

    Chapter 1 exercises

--- Chapter1.R
+++ Chapter1.R
@@ -65,8 +65,33 @@ for (i in 1:dim(gaussian.test)[2])
   
 }
 
+# Print values of A between [3,4]
+subA <- (gaussian.test[, "A"] >= 3) & (gaussian.test[, "A"] <= 4)
+trueValues <- which(subA == T)
+gaussian.test$A[trueValues]
 
+# Sample 50 rows without replacement. 
+gaussian.test[sample(50, replace = F), ] 
 
+# Bootstrap sample w/5,000 observations w/replacement and compute mean of 
+# variable. 
+colMeans(gaussian.test[sample(5000, replace = F), ])
+
+# Standardize each column. 
+scale(gaussian.test)
+
+# 1.6
+
+# Generate a data frame with 100 observations:
+# 50 "low" then 50 "high"
+# 25 "good", 25 "bad", 25 "good", 25 "bad"
+# if "low": sample from Gaussian w/mean of 2 and var. of 4
+# if "high": sample from Gaussian w/mean of 4 and var. of 1
+
+a <- factor(c(rep("low", 50), rep("high", 50)), levels = c("low", "high"))
+b <- factor(rep(c(rep("good", 25), rep("bad", 25)), 2, levels = c("good", "bad")))
+df <- data.frame(a, b, c = c(rnorm(50, mean = 2, sd = 2), 
+                             rnorm(50, mean = 4, sd = 1)))
 
 
 
