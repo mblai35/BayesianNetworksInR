98e4fd718b76f8bea81fea61c547096fb416b944
mblai35 <mbstrong35@gmail.com>
2016-12-12 17:31:45 -0700

    start of GRENITS tutorial

---
+++ GRENITStutorial.R
@@ -0,0 +1,29 @@
+# GRENITStutorial.R
+# R version 3.2.2 (2015-08-14)
+# December 12, 2016. Mallory B. Lai.
+# Walk-through of GRENITS vignette. 
+#-----------------------------------------------------------------------
+source("https://bioconductor.org/biocLite.R")
+biocLite("GRENITS")
+#-----------------------------------------------------------------------
+
+data(Athaliana_ODE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
